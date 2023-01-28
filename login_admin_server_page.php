d50aae33cf545feca199fde832112c62ad8cde0a
Aayush Gupta <vasuknlgr8@gmail.com>
2023-01-28 11:39:27 +0000

    admin portal finished

--- login_admin_server_page.php
+++ login_admin_server_page.php
@@ -1,9 +1,11 @@
 <?php
 
+include("data_class.php");
 $login_email = $_GET['login_email'];
-$login_pasword = $_GET['login_password'];
+$login_password = $_GET['login_password'];
 
 if ($login_email == null || $login_password == null) {
+
 
     $email_msg = "";
     $password_msg = "";
@@ -12,12 +14,13 @@ if ($login_email == null || $login_password == null) {
         $email_msg = "Email Empty";
     }
     if ($login_pasword == null) {
-        $password_msg = "Pasword Empty";
+        $password_msg = "Password Empty";
     }
 
     header("Location: index.php?admin_email_msg=$email_msg&admin_password_msg=$password_msg");
-} elseif ($login_email != null && $login_pasword != null) {
+} elseif ($login_email != null && $login_password != null) {
+
     $obj = new data();
     $obj->setconnection();
-    $obj->adminLogin($login_email, $login_pasword);
+    $obj->adminLogin($login_email, $login_password);
 }
